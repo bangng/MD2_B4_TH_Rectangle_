77f1ba2db2956a4cf074f8ce5d37dfccbd9f683c
bangng <montersb@gmail.com>
2022-07-27 23:19:26 +0700

    mang

---
+++ src/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+    double width, height;
+    public Rectangle(){
+
+    }
+    public Rectangle(double width, double height){
+        this.height = height;
+        this.width = width;
+
+    }
+    public double getArea(){
+        return this.height * this.width;
+    }
+    public double getPerimeter(){
+        return (this.width + this.height) * 2;
+    }
+    public String display(){
+        return "Rectangle{" + "width=" + width +",height=" +height + "}";
+    }
+}
